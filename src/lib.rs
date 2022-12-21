021df0d8f073b58ba79105f19e7dc14e72eaad49
Sean Pianka <pianka@eml.cc>
2022-12-20 23:31:32 -0500

    doc: add more doc comments to pub functions
    Signed-off-by: Sean Pianka <pianka@eml.cc>

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ lazy_static! {
     };
 }
 
+/// Describes different types of errors with supplied zipcodes during parsing.
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Invalid format, zipcode must be of the format: \"#####\" or \"#####-####\"")]
@@ -27,22 +28,31 @@ pub enum Error {
     InvalidCharacters,
 }
 
+/// A result type where the error is an `Error`.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Determine whether a supplied zipcode matches any existing zipcode. The supplied zipcode must be of the format: "#####", "#####-####", or "##### ####".
 pub fn matching(zipcode: &str, zipcodes: Option<Vec<Zipcode>>) -> Result<Vec<Zipcode>> {
     let zipcode = clean_zipcode(zipcode)?;
     let zipcodes = zipcodes.as_ref().unwrap_or(&ZIPCODES);
     let matching_zipcodes = zipcodes.iter().filter(|z| z.zip_code == zipcode).cloned().collect::<Vec<_>>();
-    debug_println!("is_real matched {:?} zipcodes", matching_zipcodes.len());
+    debug_println!("is_real matched {:?} zipcodes for {}", matching_zipcodes.len(), zipcode);
     Ok(matching_zipcodes)
 }
 
+/// Returns true if the supplied zipcode is a valid zipcode.
+///
+/// This is mainly a wrapper around `is_real` that returns a `Result` instead of a `bool`.
 pub fn is_real(zipcode: &str) -> Result<bool> {
     let zipcode = clean_zipcode(zipcode)?;
     let matching_zipcodes = matching(zipcode, None)?;
     Ok(!matching_zipcodes.is_empty())
 }
 
+/// Using a supplied list of filt-er-functions, return a filtered list of zipcodes.
+///
+/// By default, the supplied list of zipcodes is everything stored in the
+/// database. However, an optional list of override zipcodes can be supplied.
 pub fn filter_by<F>(filters: Vec<F>, zipcodes: Option<Vec<Zipcode>>) -> Result<Vec<Zipcode>>
                     where F: Fn(&Zipcode) -> bool {
     let zipcodes = zipcodes.as_ref().unwrap_or(&ZIPCODES);
@@ -56,6 +66,7 @@ pub fn filter_by<F>(filters: Vec<F>, zipcodes: Option<Vec<Zipcode>>) -> Result<V
     }).cloned().collect::<Vec<_>>())
 }
 
+/// Retrieve a list of all zipcodes in the database.
 pub fn list_all() -> Vec<Zipcode> {
     ZIPCODES.clone()
 }
